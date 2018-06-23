fbd1ddf6d5a7138a32e5e2758a21d097b7606942
Michinobu Maeda <michinobumaeda@gmail.com>
2018-06-23 20:52:14 +0900

    port to Vue, Quasar and Firebase.

--- up/src/app/auth/signup.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector:'signup',
-  templateUrl: './signup.component.html',
-})
-export class SignUpComponent {
-}
--- up/src/app/notfound.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './notfound.component.html',
-})
-export class NotFoundComponent {
-}
