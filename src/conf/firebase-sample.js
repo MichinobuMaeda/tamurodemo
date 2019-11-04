20c67391264b3900d6118897d21ab26307367b92
Michinobu Maeda <michinobumaeda@gmail.com>
2019-11-05 01:32:17 +0900

    secure firebase

---
+++ src/conf/firebase-sample.js
@@ -0,0 +1,11 @@
+const firebase = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+}
+
+export default firebase
