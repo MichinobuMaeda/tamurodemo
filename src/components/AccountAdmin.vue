95351bf673a43cc576d3a9ce1fd233c5fad5e56c
Michinobu Maeda <michinobumaeda@gmail.com>
2019-11-16 23:53:54 +0900

    add to set valid

--- src/components/AccountAdmin.vue
+++ src/components/AccountAdmin.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="row">
     <div class="col q-pa-md">
+      <div class="row" v-if="account.id !== $store.state.me.id">
+        <div class="col col-xs-4 col-sm-2 q-pa-md">
+          <q-btn :label="$t('invitation')" color="primary" @click="invite" />
+        </div>
+        <div class="col col-xs-8 col-sm-10 q-px-md">
+        <q-input v-model="account.invitationURL" readonly v-if="account.invitationURL"/>
+        </div>
+      </div>
       <q-list bordered separator>
         <q-item>
           <q-item-section>
@@ -23,18 +31,18 @@
             {{ $t('accountPaused') }}
           </q-item-section>
           <q-item-section>
-            {{ account.data().valid ? 'No' : 'Yes' }}
+            <q-toggle
+              v-model="paused"
+              :label="paused ? 'Yes' : 'No'"
+              @input="toggleValid"
+              v-if="account.id !== $store.state.me.id"
+            />
+            <span v-else>
+              {{ paused ? 'Yes' : 'No' }}
+            </span>
           </q-item-section>
         </q-item>
       </q-list>
-      <div class="row">
-        <div class="col col-xs-4 col-sm-2 q-pa-md">
-          <q-btn :label="$t('invitation')" color="primary" @click="invite(account.id)" />
-        </div>
-        <div class="col col-xs-8 col-sm-10 q-px-md">
-        <q-input v-model="account.invitationURL" readonly v-if="account.invitationURL"/>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -44,11 +52,23 @@ import { mapGetters } from 'vuex'
 
 export default {
   props: [ 'account' ],
+  data () {
+    return {
+      paused: !this.account.data().valid
+    }
+  },
   methods: {
-    async invite (id) {
+    async invite () {
+      const id = this.account.id
       let result = await this.$store.state.firebase.functions().httpsCallable('getInvitationUrl')({ id })
       this.account.invitationURL = result.data.url
       this.$forceUpdate()
+    },
+    async toggleValid () {
+      const id = this.account.id
+      await this.$store.state.db.collection('accounts').doc(id).update({
+        valid: !this.account.data().valid
+      })
     }
   },
   computed: {
