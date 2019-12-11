33d4996ce5894d136845452fb231d1e6675f4add
Michinobu Maeda <michinobumaeda@gmail.com>
2019-12-12 00:22:35 +0900

    bug fix

--- src/store/actionsService.js
+++ src/store/actionsService.js
@@ -4,7 +4,7 @@ export const releaseUiNewVersion = ({ state }) => state.db.collection('service')
   version: state.conf.version
 })
 
-const onServiceStatusChanged = ({ commit, state }, { doc, i18n }) => {
+const onServiceStatusChanged = async ({ commit, state }, { doc, i18n }) => {
   commit('setService', doc)
   i18n.locale = (state.service && state.service.status && state.service.status.locale) || i18n.locale
   if (state.service.status.version > state.conf.version) {
@@ -20,8 +20,8 @@ const onServiceStatusChanged = ({ commit, state }, { doc, i18n }) => {
 }
 
 export const getServiceStatus = async ({ commit, state }, { i18n }) => {
-  onServiceStatusChanged({ commit, state }, { doc: await state.db.collection('service').doc('status').get(), i18n })
-  state.db.collection('service').doc('status').onSnapshot(doc => {
-    onServiceStatusChanged({ commit, state }, { doc, i18n })
+  await onServiceStatusChanged({ commit, state }, { doc: await state.db.collection('service').doc('status').get(), i18n })
+  state.db.collection('service').doc('status').onSnapshot(async doc => {
+    await onServiceStatusChanged({ commit, state }, { doc, i18n })
   })
 }
