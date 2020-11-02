387a72fd3a6dd1cf5aea3da749a9c69213f4a281
Michinobu Maeda <michinobumaeda@gmail.com>
2020-11-02 19:31:17 +0900

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -43,10 +43,11 @@ const messaging = firebase.messaging()
 // [START on_background_message]
 messaging.onBackgroundMessage(function (payload) {
   // Customize notification here
+  console.log(JSON.stringify(payload, 0, 2))
   const notificationTitle = payload.notification.title || 'Tamuro'
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'img/icons/apple-touch-icon-120x120.png'
+    icon: 'https://tamuro-test01.web.app/img/icons/apple-touch-icon-120x120.png'
   }
 
   self.registration.showNotification(
