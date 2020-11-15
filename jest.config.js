3d848402be563e6a668bea1a87ca276cc0f4556b
Michinobu Maeda <michinobumaeda@gmail.com>
2020-11-15 18:29:12 +0900

    run all unit test for ci-jobs of both ui and functions

--- jest.config.js
+++ jest.config.js
@@ -4,24 +4,18 @@ module.exports = {
   collectCoverageFrom: [
     'src/auth/*.{js,jsx}',
     'src/store/*.{js,jsx}',
+    'functions/*.{js,jsx}',
     // '**/*.{js,jsx}',
     '!**/index.js',
     '!**/node_modules/**',
     '!**/tests/**',
     '!**/coverage/**',
-    '!**/*.config.js',
-    '!**/functions/**'
+    '!**/tools/**',
+    '!**/*.config.js'
   ],
   coverageDirectory: 'coverage',
   runner: 'jest-serial-runner',
-  moduleDirectories: [
-    'node_modules',
-    'src'
-  ],
   testEnvironment: 'node',
-  testMatch: [
-    '<rootDir>/tests/**/?(*.)+(spec|test).[jt]s?(x)'
-  ],
   transformIgnorePatterns: [
     'node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'
   ],
