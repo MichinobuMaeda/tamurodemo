b47f55347256bea956dcca41312cb91cc83eba69
Michinobu Maeda <michinobumaeda@gmail.com>
2021-03-07 00:13:54 +0900

    app updater: change to modal bottom sheet

--- src/components/AppUpdater.vue
+++ src/components/AppUpdater.vue
@@ -1,13 +1,26 @@
 <template>
-  <DefaultButton
-    :color="color"
-    :icon="icon"
-    :label="label"
-    @click="updateApp"
-  />
+  <v-bottom-sheet
+    inset
+    v-model="state.show"
+    persistent
+  >
+    <v-sheet
+      class="text-center pb-8"
+    >
+    <v-alert :type="color" text class="">
+      {{ label }}
+    </v-alert>
+    <DefaultButton
+      :color="color"
+      label="OK"
+      @click="updateApp"
+    />
+    </v-sheet>
+  </v-bottom-sheet>
 </template>
 
 <script>
+import { reactive } from '@vue/composition-api'
 import DefaultButton from './DefaultButton'
 
 export default {
@@ -30,6 +43,9 @@ export default {
     }
   },
   setup () {
+    const state = reactive({
+      show: true
+    })
     const updateApp = async () => {
       const registrations = await navigator.serviceWorker.getRegistrations()
       registrations.forEach(registration => {
@@ -38,6 +54,7 @@ export default {
       document.location.reload(true)
     }
     return {
+      state,
       updateApp
     }
   }
